987aa009efb04266fa4cc25b4f587f6505a368c0
sarvadnya2030 <sarvadnya.bhatlawande2005@gmail.com>
2025-02-01 16:41:47 +0530

    Initial commit

---
+++ .dist/main.js
@@ -0,0 +1,12 @@
+
+$(function(){
+    $("#btn").click(function(){
+        $("#h1-text").html("edureka");
+    });
+});
+$(function() {
+    console.log("jQuery is working!");
+});
+//jquery syntax: $();
+//$(function(){})
+
